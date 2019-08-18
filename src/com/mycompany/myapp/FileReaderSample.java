d7081b803489c3486ae0f1895186b07f688f1c66
testuser-platform <54242763+testuser-platform@users.noreply.github.com>
2019-08-18 20:57:10 +0530

    Initial Commit

---
+++ src/com/mycompany/myapp/FileReaderSample.java
@@ -0,0 +1,29 @@
+package com.mycompany.myapp;
+
+import java.io.FileReader;
+import java.io.FileWriter;
+
+public class FileReaderSample {
+
+	 public static void main(String args[]) throws IOException {
+	      FileReader in = null;
+	      FileWriter out = null;
+
+	      try {
+	         }
+	         in = new FileReader("input.txt");
+	         out = new FileWriter("output.txt");
+	         
+	         int c;
+	         while ((c = in.read()) != -1) {
+	            out.write(c);
+	      }finally {
+	         if (in != null) {
+	            in.close();
+	         }
+	         if (out != null) {
+	            out.close();
+	         }
+	      }
+	   }
+}
